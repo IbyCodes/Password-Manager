a577af14d1b820ab0f6d1d80aa20cfc15271a903
MohammadKhan <ibrahimkhan786007@gmail.com>
2022-07-23 14:21:15 -0600

    Deleted the application.css file
    Deleted stylesheet code
    
    Set the title for the window

--- src/application/Main.java
+++ src/application/Main.java
@@ -12,8 +12,11 @@ public class Main extends Application {
 		try {
 			BorderPane root = new BorderPane();
 			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			
 			primaryStage.setScene(scene);
+			
+			primaryStage.setTitle("Password Manager");
+			
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
