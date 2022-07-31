5255be8b91f07d3234ad215e36623dc7bb71bc37
MohammadKhan <ibrahimkhan786007@gmail.com>
2022-07-30 19:59:23 -0600

    Created a new class for the master password conditions

---
+++ src/application/MasterPasswordConditions.java
@@ -0,0 +1,13 @@
+package application;
+
+public class MasterPasswordConditions { // created a new class for the master password conditions (to check)
+
+	public MasterPasswordConditions() {
+		
+		
+		
+		
+		
+	}
+
+}
