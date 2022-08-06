a9d9d2f471579e288967aa788392a22ce80018a1
Acer-PC <Acer-PC@10.0.0.240>
2022-08-06 14:52:16 -0600

    error with changing scenes

--- src/application/VerifyMasterPasswordController.java
+++ src/application/VerifyMasterPasswordController.java
@@ -1,10 +1,16 @@
 package application;
 
+import java.io.IOException;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.text.Text;
+import javafx.stage.Stage;
 
 public class VerifyMasterPasswordController extends MasterPasswordController {
 
@@ -25,17 +31,25 @@ public class VerifyMasterPasswordController extends MasterPasswordController {
     
     
     @FXML
-    void VerifyMasterPass(ActionEvent event) {
+    void VerifyMasterPass() throws IOException {
     	
     	String inputToCheck = VerifyMasterPasswordField.getText();
+    	
     	if(inputToCheck != getMasterPassword()) {	
     		
-    		UnlockManagerPassError.setText("The master password input is incorrect. Try again.");
-    		
+    		// UnlockManagerPassError.setText("The master password input is incorrect. Try again."); giving error
+
     	}
     	
     	else {
-    		; // here we can switch the scene to scene AccessUsernameAndPasswordView
+    		
+    		// VerifyMasterPasswordButton.setOnAction(e -> swtichScenes("AccessUsernameAndPasswordView.fxml"));
+    	
+    		Parent root = FXMLLoader.load(getClass().getResource("AccessUsernameAndPasswordView.fxml"));
+    		Scene accessGranted = new Scene(root);
+    		Stage settingUpStage = new Stage();
+    		settingUpStage.setScene(accessGranted);
+    		settingUpStage.show();
     		
     	}
     	
